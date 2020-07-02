cc2db0753eaec4b33a9ff6eadfd763e942290308
mikewolfd <michael.f.deeb@gmail.com>
2020-07-02 08:54:28 -0500

    downloaded relevant javascript files and serve them directly

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,8 @@ function toggleScrollClass() {
     let nav = document.getElementsByClassName('nav-bread')[0];
 
     if (nav.getBoundingClientRect().y > 0) {
-        $('.nav-bread .breadcrumb').removeClass('fadein').addClass('fadeout')
+        $('.nav-bread').removeClass('fadein').addClass('fadeout')
     } else {
-        $('.nav-bread .breadcrumb').addClass('fadein').removeClass('fadeout')
+        $('.nav-bread').addClass('fadein').removeClass('fadeout')
     }
 }

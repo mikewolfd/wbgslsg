3bf1176d9568a02cd3fb2cffb11a3116eef188d0
mikewolfd <michael.f.deeb@gmail.com>
2020-07-08 10:55:06 -0500

    rebuild

--- dist/assets/js/principle_e_4.js
+++ dist/assets/js/principle_e_4.js
@@ -17,8 +17,8 @@
                     /* always keep at least 1 open by preventing the current to close itself */
                     $('[data-toggle="collapse"]').on('click', function (e) {
                         if (true === JSON.parse(e.currentTarget.getAttribute('aria-expanded'))) {
-                                e.stopPropagation();
+                            e.stopPropagation();
                         }
-                })
-            });
+                    })
+                });
             
